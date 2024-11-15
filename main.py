f6133fa0a8c5eeb3ee081d9fec165bc2010156b1
Ali Gardezi <sgardezi@google.com>
2024-11-15 23:50:17 +0000

    temp control added

--- main.py
+++ main.py
@@ -10,9 +10,24 @@ def increment_counter():
 
 if 'count' not in st.session_state:
     st.session_state.count = 0
+
+if "celsius" not in st.session_state:
+    # set the initial default value of the slider widget
+    st.session_state.celsius = 50.0
+
+
  
 
 st.button('Increment Even', on_click=increment_counter)
 
 st.write('Count = ', st.session_state.count)
 
+st.slider(
+    "Temperature in Celsius",
+    min_value=-100.0,
+    max_value=100.0,
+    key="celsius"
+)
+
+# This will get the value of the slider widget
+st.write(st.session_state.celsius)

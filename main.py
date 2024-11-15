5946548cb3fddd896f51a140db6c299feeaebfd5
Ali Gardezi <sgardezi@google.com>
2024-11-15 18:37:19 +0000

    Added button

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ st.write("Hello world")
 st.title("Sample AI App")
 
 st.text("This is a sample app.")
+
+count = 0
+
+increment = st.button('Increment')
+if increment:
+    count += 1
+
+st.write('Count = ', count)

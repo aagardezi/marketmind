7202dbb87de35afd7ce8b479849b6ea36489720b
Ali Gardezi <sgardezi@google.com>
2024-11-16 00:13:01 +0000

    chat added

--- main.py
+++ main.py
@@ -31,3 +31,17 @@ st.slider(
 
 # This will get the value of the slider widget
 st.write(st.session_state.celsius)
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})

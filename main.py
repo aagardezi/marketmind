a44e95d2f799b2a0edc3ef9c361fbc16a3eef567
Ali Gardezi <sgardezi@google.com>
2024-11-16 23:21:13 +0000

    bq connection added

--- main.py
+++ main.py
@@ -1,5 +1,80 @@
+import time
 import streamlit as st
-from vertexai.generative_models import GenerativeModel, Part, FinishReason, SafetySetting
+from vertexai.generative_models import FunctionDeclaration, GenerativeModel, Tool, Part, FinishReason, SafetySetting
+from google.cloud import bigquery
+
+BIGQUERY_DATASET_ID = "dbd-sdlc-prod"
+
+list_datasets_func = FunctionDeclaration(
+    name="list_datasets",
+    description="Get a list of datasets that will help answer the user's question",
+    parameters={
+        "type": "object",
+        "properties": {},
+    },
+)
+
+list_tables_func = FunctionDeclaration(
+    name="list_tables",
+    description="List tables in a dataset that will help answer the user's question",
+    parameters={
+        "type": "object",
+        "properties": {
+            "dataset_id": {
+                "type": "string",
+                "description": "Dataset ID to fetch tables from.",
+            }
+        },
+        "required": [
+            "dataset_id",
+        ],
+    },
+)
+
+get_table_func = FunctionDeclaration(
+    name="get_table",
+    description="Get information about a table, including the description, schema, and number of rows that will help answer the user's question. Always use the fully qualified dataset and table names.",
+    parameters={
+        "type": "object",
+        "properties": {
+            "table_id": {
+                "type": "string",
+                "description": "Fully qualified ID of the table to get information about",
+            }
+        },
+        "required": [
+            "table_id",
+        ],
+    },
+)
+
+sql_query_func = FunctionDeclaration(
+    name="sql_query",
+    description="Get information from data in BigQuery using SQL queries",
+    parameters={
+        "type": "object",
+        "properties": {
+            "query": {
+                "type": "string",
+                "description": f"SQL query on a single line that will help give quantitative answers to the user's question when run on a BigQuery dataset and table. In the SQL query, always use the fully qualified dataset and table names.
+                When writing SQL query ensure you use the Date_Time field in the where clause. LSE_NORMALISED table is the main trade table
+                RIC is the column to search for a stock",
+            }
+        },
+        "required": [
+            "query",
+        ],
+    },
+)
+
+sql_query_tool = Tool(
+    function_declarations=[
+        list_datasets_func,
+        list_tables_func,
+        get_table_func,
+        sql_query_func,
+    ],
+)
 
 st.write("Hello world")
 
@@ -44,7 +119,9 @@ safety_settings = [
 
 model = GenerativeModel(
     "gemini-1.5-flash-002",
-    #system_instruction=["""answer in pirate lingo"""]
+    system_instruction=["""You are a financial analysit that understands lseg tick history data and uses RIC codes to analyse stocks
+    When writing SQL query ensure you use the Date_Time field in the where clause. LSE_NORMALISED table is the main trade table"""],
+    tools=[sql_query_tool],
 )
 
 st.button('Increment Even', on_click=increment_counter)
@@ -71,6 +148,9 @@ for message in st.session_state.messages:
 if "chat" not in st.session_state:
     st.session_state.chat = model.start_chat()
 
+if "client" not in st.session_state:
+    st.session_state.bqclient = bigquery.Client()
+
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     with st.chat_message("user"):
@@ -84,9 +164,156 @@ if prompt := st.chat_input("What is up?"):
         response = st.session_state.chat.send_message(prompt,generation_config=generation_config,
         safety_settings=safety_settings)
         
-        #response = response.candidates[0].content.parts[0]
+        response = response.candidates[0].content.parts[0]
+
+        api_requests_and_responses = []
+        backend_details = ""
+
+        function_calling_in_process = True
+        while function_calling_in_process:
+            try
+                params = {}
+                for key, value in response.function_call.args.items():
+                    params[key] = value
+            
+                print(response.function_call.name)
+                print(params)
+
+                if response.function_call.name == "list_datasets":
+                    api_response = st.session_state.client.list_datasets()
+                    api_response = BIGQUERY_DATASET_ID
+                    api_requests_and_responses.append(
+                        [response.function_call.name, params, api_response]
+                    )
+
+                if response.function_call.name == "list_tables":
+                    api_response = st.session_state.client.list_tables(params["dataset_id"])
+                    api_response = str([table.table_id for table in api_response])
+                    api_requests_and_responses.append(
+                        [response.function_call.name, params, api_response]
+                    )
+
+                if response.function_call.name == "get_table":
+                    api_response = st.session_state.client.get_table(params["table_id"])
+                    api_response = api_response.to_api_repr()
+                    api_requests_and_responses.append(
+                        [
+                            response.function_call.name,
+                            params,
+                            [
+                                str(api_response.get("description", "")),
+                                str(
+                                    [
+                                        column["name"]
+                                        for column in api_response["schema"][
+                                            "fields"
+                                        ]
+                                    ]
+                                ),
+                            ],
+                        ]
+                    )
+                    api_response = str(api_response)
+
+                if response.function_call.name == "sql_query":
+                    job_config = bigquery.QueryJobConfig(
+                        maximum_bytes_billed=100000000
+                    )  # Data limit per query job
+                    try:
+                        cleaned_query = (
+                            params["query"]
+                            .replace("\\n", " ")
+                            .replace("\n", "")
+                            .replace("\\", "")
+                        )
+                        query_job = st.session_state.client.query(
+                            cleaned_query, job_config=job_config
+                        )
+                        api_response = query_job.result()
+                        api_response = str([dict(row) for row in api_response])
+                        api_response = api_response.replace("\\", "").replace(
+                            "\n", ""
+                        )
+                        api_requests_and_responses.append(
+                            [response.function_call.name, params, api_response]
+                        )
+                    except Exception as e:
+                        error_message = f"""
+                        We're having trouble running this SQL query. This
+                        could be due to an invalid query or the structure of
+                        the data. Try rephrasing your question to help the
+                        model generate a valid query. Details:
+
+                        {str(e)}"""
+                        st.error(error_message)
+                        api_response = error_message
+                        api_requests_and_responses.append(
+                            [response.function_call.name, params, api_response]
+                        )
+                        st.session_state.messages.append(
+                            {
+                                "role": "assistant",
+                                "content": error_message,
+                            }
+                        )
+
+                print(api_response)
+
+                response = st.session_state.chat.send_message(
+                    Part.from_function_response(
+                        name=response.function_call.name,
+                        response={
+                            "content": api_response,
+                        },
+                    ),
+                )
+                response = response.candidates[0].content.parts[0]
+
+                backend_details += "- Function call:\n"
+                backend_details += (
+                    "   - Function name: ```"
+                    + str(api_requests_and_responses[-1][0])
+                    + "```"
+                )
+                backend_details += "\n\n"
+                backend_details += (
+                    "   - Function parameters: ```"
+                    + str(api_requests_and_responses[-1][1])
+                    + "```"
+                )
+                backend_details += "\n\n"
+                backend_details += (
+                    "   - API response: ```"
+                    + str(api_requests_and_responses[-1][2])
+                    + "```"
+                )
+                backend_details += "\n\n"
+                with message_placeholder.container():
+                    st.markdown(backend_details)
+
+            except AttributeError:
+                function_calling_in_process = False
+
+        time.sleep(3)
+
+        full_response = response.text
         with message_placeholder.container():
-            message_placeholder.markdown(response)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+            st.markdown(full_response.replace("$", r"\$"))  # noqa: W605
+            with st.expander("Function calls, parameters, and responses:"):
+                st.markdown(backend_details)
+
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": full_response,
+                "backend_details": backend_details,
+            }
+        )
+
+
+
+        # with message_placeholder.container():
+        #     message_placeholder.markdown(response.text)
+        # st.session_state.messages.append({"role": "assistant", "content": response.text})
 
     

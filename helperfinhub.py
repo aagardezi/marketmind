b75a064ea72bf484e76c8592f1326acbc5b98792
Ali Gardezi <sgardezi@google.com>
2024-11-20 00:02:59 +0000

    finhub symbol code fixed

--- helperfinhub.py
+++ helperfinhub.py
@@ -5,59 +5,59 @@ finnhub_client = finnhub.Client(api_key="cstluhpr01qj0ou20db0cstluhpr01qj0ou20db
 def symbol_lookup(params):
     return finnhub_client.symbol_lookup(params['company_name'])
 
-def get_quote(symbol):
-    return finnhub_client.quote(symbol)
+def get_quote(params):
+    return finnhub_client.quote(params['symbol'])
 
 def company_news(params):
     return finnhub_client.company_news(params['symbol'], _from=params['from_date'], to=params['to_date'])
 
-def news_sentiment(symbol):
-    return finnhub_client.news_sentiment(symbol)
+def news_sentiment(params):
+    return finnhub_client.news_sentiment(params['symbol'])
 
-def company_peers(symbol):
-    return finnhub_client.company_peers(symbol)
+def company_peers(params):
+    return finnhub_client.company_peers(params['symbol'])
 
-def recommendation_trends(symbol):
-    return finnhub_client.recommendation_trends(symbol)
+def recommendation_trends(params):
+    return finnhub_client.recommendation_trends(params['symbol'])
 
-def recommendation_trend(symbol):
-    return finnhub_client.recommendation_trend(symbol)
+def recommendation_trend(params):
+    return finnhub_client.recommendation_trend(params['symbol'])
 
-def recommendation_earnings(symbol):
-    return finnhub_client.recommendation_earnings(symbol)
+def recommendation_earnings(params):
+    return finnhub_client.recommendation_earnings(params['symbol'])
 
-def recommendation_insider(symbol):
-    return finnhub_client.recommendation_insider(symbol)
+def recommendation_insider(params):
+    return finnhub_client.recommendation_insider(params['symbol'])
 
-def recommendation_insider_sentiment(symbol):
-    return finnhub_client.recommendation_insider_sentiment(symbol)
+def recommendation_insider_sentiment(params):
+    return finnhub_client.recommendation_insider_sentiment(params['symbol'])
 
-def recommendation_insider_trading(symbol):
-    return finnhub_client.recommendation_insider_trading(symbol)
+def recommendation_insider_trading(params):
+    return finnhub_client.recommendation_insider_trading(params['symbol'])
 
-def company_executive(symbol):
-    return finnhub_client.company_executive(symbol)
+def company_executive(params):
+    return finnhub_client.company_executive(params['symbol'])
 
-def company_profile(symbol):
-    return finnhub_client.company_profile2(symbol)
+def company_profile(params):
+    return finnhub_client.company_profile2(params['symbol'])
 
-def company_profile2(symbol):
-    return finnhub_client.company_profile2(symbol)
+def company_profile2(params):
+    return finnhub_client.company_profile2(params['symbol'])
 
-def index_constituents(symbol):
-    return finnhub_client.index_constituents(symbol)
+def index_constituents(params):
+    return finnhub_client.index_constituents(params['symbol'])
 
-def index_profile(symbol):
-    return finnhub_client.index_profile(symbol)
+def index_profile(params):
+    return finnhub_client.index_profile(params['symbol'])
 
-def index_constituents_exchanges(symbol):
-    return finnhub_client.index_constituents_exchanges(symbol)
+def index_constituents_exchanges(params):
+    return finnhub_client.index_constituents_exchanges(params['symbol'])
 
-def index_constituents_prices(symbol):
-    return finnhub_client.index_constituents_prices(symbol)
+def index_constituents_prices(params):
+    return finnhub_client.index_constituents_prices(params['symbol'])
 
-def index_constituents_profiles(symbol):
-    return finnhub_client.index_constituents_profiles(symbol)
+def index_constituents_profiles(params):
+    return finnhub_client.index_constituents_profiles(params['symbol'])
 
 function_handler = {
     "symbol_lookup": symbol_lookup,

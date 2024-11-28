1c928e4041b8851f4e0e90ea3476bceba386239a
Ali Gardezi <sgardezi@google.com>
2024-11-28 12:00:50 +0000

    Project ID fixed

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import vertexai
 from vertexai.generative_models import FunctionDeclaration, GenerativeModel, Tool, Part, FinishReason, SafetySetting
 from google.cloud import bigquery
 from google.cloud import secretmanager
+import google.auth
 import logging
 
 import helpergetnews
@@ -18,9 +19,6 @@ import geminifunctionfinhub
 import geminifunctiongetnews
 import helperfinhub
 
-
-BIGQUERY_DATASET_ID = "lseg_data_normalised"
-PROJECT_ID = "genaillentsearch"
 
 st.set_page_config(layout="wide")
 float_init(theme=True, include_unstable_primary=False)
@@ -40,7 +38,23 @@ def select_model():
         st.rerun()
 
 
+def get_project_id():
+  """Gets the current GCP project ID.
 
+  Returns:
+    The project ID as a string.
+  """
+
+  try:
+    _, project_id = google.auth.default()
+    return project_id
+  except google.auth.exceptions.DefaultCredentialsError as e:
+    print(f"Error: Could not determine the project ID. {e}")
+    return None
+
+
+BIGQUERY_DATASET_ID = "lseg_data_normalised"
+PROJECT_ID = get_project_id()
 
 sql_query_tool = Tool(
     function_declarations=[
@@ -201,7 +215,7 @@ if st.session_state['connected']:
             logging.warning("model name session state initialised")
 
             st.title(f"""Hello: {st.session_state['user_info'].get('name')}! Company Agent: built using {st.session_state.modelname}""")
-            vertexai.init(project="fisd-hackathon24lon-9675", location="us-central1")
+            vertexai.init(project=PROJECT_ID, location="us-central1")
             model = GenerativeModel(
                 # "gemini-1.5-pro-002",
                 st.session_state.modelname,
